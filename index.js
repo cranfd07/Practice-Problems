d44891c5f1df4e2079426eb57a504810be7b77d9
Clark Cranford <b.cranford07@gmail.com>
2022-08-27 17:49:44 -0400

    Codewars

--- index.js
+++ index.js
@@ -1,1 +1,79 @@
 console.log('Hello');
+
+/*function descendingOrder(n){
+    //create newString use the methods - toString - split
+   
+    const newString = n.toString().split('');
+    //sort method b - a
+    const finalArray = newString.sort();
+    console.log(finalArray)
+    return parseInt(finalArray.reverse().join(''));
+  }
+
+  console.log(descendingOrder(111));
+  console.log(descendingOrder(15));
+  console.log(descendingOrder(121));
+  console.log(descendingOrder(123456789));*/
+
+  /*function alphanumeric(string){
+    //create an object with all passable values 
+    const obj = {
+        0: true,
+        1: true,
+        2: true,
+        3: true,
+        4: true,
+        5: true,
+        6: true,
+        7: true,
+        8: true,
+        9: true,
+        a: true,
+        b: true,
+        c: true,
+        d: true,
+        e: true,
+        f: true,
+        g: true,
+        h: true,
+        i: true,
+        j: true,
+        k: true,
+        l: true,
+        m: true,
+        n: true,
+        o: true,
+        p: true,
+        q: true,
+        r: true,
+        s: true,
+        t: true,
+        u: true,
+        v: true,
+        w: true,
+        x: true,
+        y: true,
+        z: true,
+
+    }
+    if (string[0] === undefined) return false;
+    //for loop over the input string 
+    for (let i = 0; i < string.length; i++) {
+    //conditional: if string[i].toLowerCase() !== key on the object then return false 
+    if (!obj[string[i].toLowerCase()]) {
+        return false;
+    }
+    }
+    //outside of the for loop
+    //return true;
+    return true;
+  }
+
+  console.log(alphanumeric("Mazinkaiser"));
+  console.log(alphanumeric("hello world_"));
+  console.log(alphanumeric("PassW0rd"));
+  console.log(alphanumeric("       "));*/
+
+  
+
+  
