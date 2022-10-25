c354e75accff69e302503abcadc8329128ab0a62
Clark Cranford <b.cranford07@gmail.com>
2022-10-24 21:43:42 -0400

    LC

--- index.js
+++ index.js
@@ -464,5 +464,10 @@ console.log(pivotIndex([1,7,3,6,5,6]))
 console.log(pivotIndex([1,7,3,6,5,6]))
 console.log(pivotIndex([1,7,3,6,5,6]))
 console.log(pivotIndex([1,7,3,6,5,6]))
+console.log(pivotIndex([1,7,3,6,5,6]))
+console.log(pivotIndex([1,7,3,6,5,6]))
+console.log(pivotIndex([1,7,3,6,5,6]))
+console.log(pivotIndex([1,7,3,6,5,6]))
+console.log(pivotIndex([1,7,3,6,5,6]))
 
 
