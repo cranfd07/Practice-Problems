3f5c5132356f6afe25ee34c9213f64c43968871a
Buddy <b.cranford07@gmail.com>
2022-12-30 09:46:27 -0500

    OOP

--- index.js
+++ index.js
@@ -684,3 +684,26 @@ console.log(shuffleCards(topHalf, bottomHalf));
         'Eight of Clubs',
       ]
   */
+
+const personStore = {
+  greet: function () {
+    console.log("hello");
+  },
+  introduce: function () {
+    console.log(`Hi, my name is ${sandra.name}`);
+  },
+};
+
+function personFromPersonStore(name, age) {
+  const person = Object.create(personStore);
+  person.name = name;
+  person.age = age;
+
+  return person;
+}
+
+const sandra = personFromPersonStore("Sandra", 26);
+// add code here
+
+// Uncomment this line to check your work!
+sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
